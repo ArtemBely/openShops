3415f8246d1c54240833636e3e6cb0660329bdff
Dmitry Shishkin <79608355+ihopeyoucanfly@users.noreply.github.com>
2022-11-24 17:32:47 +0300

    .

--- src/components/About/Vacancies.tsx
+++ src/components/About/Vacancies.tsx
@@ -1,39 +1,50 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export const Vacancies = () => {
-
-    return (
-      <div className='wrap_vacancies'>
-          <div className='vacancies'>
-                <p className='txt_about1Title second_title'>Вакансии</p>
-                <div className='wrap_each_vacancy'>
-                    <NavLink to='/about/1' className='each_vacancy'>
-                        <div className='wrap_vacancy_txt'>
-                            <p className='name_of'>Название вакансии</p>
-                            <p className='lastname_of'>Церковная архитектура Византии привлекает к себе значительно большее внимание,
-                             чем светская. Этому способствовали как лучшая сохранность церковных памятников в силу </p>
-                        </div>
-                        <p className='date_vacancy'>12 июля 2023</p>
-                    </NavLink>
-                    <NavLink to='/about/2' className='each_vacancy'>
-                        <div className='wrap_vacancy_txt'>
-                            <p className='name_of'>Название вакансии</p>
-                            <p className='lastname_of'>Церковная архитектура Византии привлекает к себе значительно большее внимание,
-                             чем светская. Этому способствовали как лучшая сохранность церковных памятников в силу </p>
-                        </div>
-                        <p className='date_vacancy'>12 июля 2023</p>
-                    </NavLink>
-                    <NavLink to='/about/3' className='each_vacancy'>
-                        <div className='wrap_vacancy_txt'>
-                            <p className='name_of'>Название вакансии</p>
-                            <p className='lastname_of'>Церковная архитектура Византии привлекает к себе значительно большее внимание,
-                             чем светская. Этому способствовали как лучшая сохранность церковных памятников в силу </p>
-                        </div>
-                        <p className='date_vacancy'>12 июля 2023</p>
-                    </NavLink>
-                </div>
-          </div>
+  return (
+    <div className="wrap_vacancies">
+      <div className="vacancies">
+        <p className="txt_about1Title second_title">Вакансии</p>
+        <div className="wrap_each_vacancy">
+          <NavLink to="/about/1" className="each_vacancy">
+            <div className="wrap_vacancy_txt">
+              <p className="name_of">Название вакансии 1</p>
+              <p className="lastname_of">
+                {" "}
+                Здесь представлен текст из начала описания вакансии 1. Текст
+                представлен несколькими предложениями из начала описания
+                вакансии
+              </p>
+            </div>
+            <p className="date_vacancy">12 июля 2023</p>
+          </NavLink>
+          <NavLink to="/about/2" className="each_vacancy">
+            <div className="wrap_vacancy_txt">
+              <p className="name_of">Название вакансии 2</p>
+              <p className="lastname_of">
+                {" "}
+                Здесь представлен текст из начала описания вакансии 2. Текст
+                представлен несколькими предложениями из начала описания
+                вакансии
+              </p>
+            </div>
+            <p className="date_vacancy">12 июля 2023</p>
+          </NavLink>
+          <NavLink to="/about/3" className="each_vacancy">
+            <div className="wrap_vacancy_txt">
+              <p className="name_of">Название вакансии 3</p>
+              <p className="lastname_of">
+                {" "}
+                Здесь представлен текст из начала описания вакансии 3. Текст
+                представлен несколькими предложениями из начала описания
+                вакансии
+              </p>
+            </div>
+            <p className="date_vacancy">12 июля 2023</p>
+          </NavLink>
+        </div>
       </div>
-    )
-}
+    </div>
+  );
+};
--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,42 +1,47 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
-const fone1 = '../../../images/Rectangle 14-4.png';
-const fone2 = '../../../images/Rectangle 14-2-2.png';
-const fone3 = '../../../images/Rectangle 14-3-2.png';
+import { NavLink } from "react-router-dom";
+const fone1 = "../../../images/Rectangle 14-4.png";
+const fone2 = "../../../images/Rectangle 14-2-2.png";
+const fone3 = "../../../images/Rectangle 14-3-2.png";
 
 export const News = () => {
-
-    return (
-      <div className='wrap_buildings_inside'>
-          <div className='news_inside'>
-              <NavLink to='/news/1' className='wrap_each_news'>
-                  <img src={fone1} className='each_news_img'/>
-                  <div className='wrap_description_of_news'>
-                    <p className='title_of_news'>Название новости</p>
-                    <p className='description_of_news'>Церковная архитектура Византии привлекает к себе значительно большее внимание, чем светская.
-                     Этому способствовали как лучшая сохранность церковных памятников в силу </p>
-                  </div>
-                  <p className='date_style'>12 июля 2023</p>
-              </NavLink>
-              <NavLink to='/news/2' className='wrap_each_news'>
-                  <img src={fone2} className='each_news_img'/>
-                  <div className='wrap_description_of_news'>
-                    <p className='title_of_news'>Название новости</p>
-                    <p className='description_of_news'>Церковная архитектура Византии привлекает к себе значительно большее внимание, чем светская.
-                     Этому способствовали как лучшая сохранность церковных памятников в силу </p>
-                  </div>
-                  <p className='date_style'>12 июля 2023</p>
-              </NavLink>
-              <NavLink to='/news/3' className='wrap_each_news'>
-                  <img src={fone3} className='each_news_img'/>
-                  <div className='wrap_description_of_news'>
-                    <p className='title_of_news'>Название новости</p>
-                    <p className='description_of_news'>Церковная архитектура Византии привлекает к себе значительно большее внимание, чем светская.
-                     Этому способствовали как лучшая сохранность церковных памятников в силу </p>
-                  </div>
-                  <p className='date_style'>12 июля 2023</p>
-              </NavLink>
+  return (
+    <div className="wrap_buildings_inside">
+      <div className="news_inside">
+        <NavLink to="/news/1" className="wrap_each_news">
+          <img src={fone1} className="each_news_img" />
+          <div className="wrap_description_of_news">
+            <p className="title_of_news">Название новости 1</p>
+            <p className="description_of_news">
+              Здесь представлен текст из начала описания новости 1. Текст
+              представлен несколькими предложениями из начала описания новости
+            </p>
           </div>
+          <p className="date_style">12 июля 2023</p>
+        </NavLink>
+        <NavLink to="/news/2" className="wrap_each_news">
+          <img src={fone2} className="each_news_img" />
+          <div className="wrap_description_of_news">
+            <p className="title_of_news">Название новости 2</p>
+            <p className="description_of_news">
+              Здесь представлен текст из начала описания новости 2. Текст
+              представлен несколькими предложениями из начала описания новости
+            </p>
+          </div>
+          <p className="date_style">12 июля 2023</p>
+        </NavLink>
+        <NavLink to="/news/3" className="wrap_each_news">
+          <img src={fone3} className="each_news_img" />
+          <div className="wrap_description_of_news">
+            <p className="title_of_news">Название новости 3</p>
+            <p className="description_of_news">
+              Здесь представлен текст из начала описания новости 3. Текст
+              представлен несколькими предложениями из начала описания новости
+            </p>
+          </div>
+          <p className="date_style">12 июля 2023</p>
+        </NavLink>
       </div>
-    )
-}
+    </div>
+  );
+};
