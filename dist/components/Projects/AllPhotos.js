932347dc7805b2f6670c6a6c499bb11ff583f4dd
Артем Белышев <you@example.com>
2022-12-05 19:42:11 +0100

    adaptive projects

--- dist/components/Projects/AllPhotos.js
+++ dist/components/Projects/AllPhotos.js
@@ -3,8 +3,8 @@ const fone1 = "../../../images/Rectangle 14-6-2.png";
 export const AllPhotos = () => {
     return (React.createElement("div", { className: 'wrap_allPhotos' },
         React.createElement("div", { className: 'allPhotos' },
-            React.createElement("p", { className: 'txt_about1Title' }, "\u0410\u0440\u0445\u0438\u0442\u0435\u043A\u0442\u0443\u0440\u043D\u043E-\u0433\u0440\u0430\u0434\u043E\u0441\u0442\u0440\u043E\u0438\u0442\u0435\u043B\u044C\u043D\u0430\u044F \u043A\u043E\u043D\u0446\u0435\u043F\u0446\u0438\u044F \u043C\u043D\u043E\u0433\u043E\u0444\u0443\u043D\u043A\u0446\u0438\u043E\u043D\u0430\u043B\u044C\u043D\u043E\u0433\u043E \u043A\u043E\u043C\u043F\u043B\u0435\u043A\u0441\u0430 \u0441 \u043F\u043E\u0434\u0437\u0435\u043C\u043D\u043E\u0439 \u0430\u0432\u0442\u043E\u0441\u0442\u043E\u044F\u043D\u043A\u043E\u0439 \u043D\u0430 \u0412\u0438\u043D\u043E\u0433\u0440\u0430\u0434\u043D\u043E\u0439 \u0443\u043B\u0438\u0446\u0435"),
-            React.createElement("p", { className: 'date_style spec_data_style' }, "12 \u0438\u044E\u043B\u044F 2023"),
+            React.createElement("p", { className: 'txt_about1Title', id: 'txt_about1TitleId' }, "\u0410\u0440\u0445\u0438\u0442\u0435\u043A\u0442\u0443\u0440\u043D\u043E-\u0433\u0440\u0430\u0434\u043E\u0441\u0442\u0440\u043E\u0438\u0442\u0435\u043B\u044C\u043D\u0430\u044F \u043A\u043E\u043D\u0446\u0435\u043F\u0446\u0438\u044F \u043C\u043D\u043E\u0433\u043E\u0444\u0443\u043D\u043A\u0446\u0438\u043E\u043D\u0430\u043B\u044C\u043D\u043E\u0433\u043E \u043A\u043E\u043C\u043F\u043B\u0435\u043A\u0441\u0430 \u0441 \u043F\u043E\u0434\u0437\u0435\u043C\u043D\u043E\u0439 \u0430\u0432\u0442\u043E\u0441\u0442\u043E\u044F\u043D\u043A\u043E\u0439 \u043D\u0430 \u0412\u0438\u043D\u043E\u0433\u0440\u0430\u0434\u043D\u043E\u0439 \u0443\u043B\u0438\u0446\u0435"),
+            React.createElement("p", { className: 'date_style spec_data_style', id: 'spec_data_style' }, "12 \u0438\u044E\u043B\u044F 2023"),
             React.createElement("div", { className: 'wrap_allImages' },
                 React.createElement("img", { src: fone1, className: 'each_photo_project first_photo_project' }),
                 React.createElement("img", { src: fone1, className: 'each_photo_project' }),
@@ -19,10 +19,10 @@ export const AllPhotos = () => {
                 React.createElement("img", { src: fone1, className: 'each_photo_project' }),
                 React.createElement("img", { src: fone1, className: 'each_photo_project' }),
                 React.createElement("img", { src: fone1, className: 'each_photo_project' }),
-                React.createElement("img", { src: fone1, className: 'each_photo_project' }),
-                React.createElement("img", { src: fone1, className: 'each_photo_project' }),
-                React.createElement("img", { src: fone1, className: 'each_photo_project' }),
-                React.createElement("img", { src: fone1, className: 'each_photo_project' }),
-                React.createElement("img", { src: fone1, className: 'each_photo_project' }),
-                React.createElement("img", { src: fone1, className: 'each_photo_project' })))));
+                React.createElement("img", { src: fone1, className: 'each_photo_project hideMobile' }),
+                React.createElement("img", { src: fone1, className: 'each_photo_project hideMobile' }),
+                React.createElement("img", { src: fone1, className: 'each_photo_project hideMobile' }),
+                React.createElement("img", { src: fone1, className: 'each_photo_project hideMobile' }),
+                React.createElement("img", { src: fone1, className: 'each_photo_project hideMobile' }),
+                React.createElement("img", { src: fone1, className: 'each_photo_project hideMobile' })))));
 };
--- src/components/Projects/AllPhotos.tsx
+++ src/components/Projects/AllPhotos.tsx
@@ -6,8 +6,8 @@ export const AllPhotos = () => {
     return (
       <div className='wrap_allPhotos'>
           <div className='allPhotos'>
-            <p className='txt_about1Title'>Архитектурно-градостроительная концепция многофункционального комплекса с подземной автостоянкой на Виноградной улице</p>
-            <p className='date_style spec_data_style'>12 июля 2023</p>
+            <p className='txt_about1Title' id='txt_about1TitleId'>Архитектурно-градостроительная концепция многофункционального комплекса с подземной автостоянкой на Виноградной улице</p>
+            <p className='date_style spec_data_style' id='spec_data_style'>12 июля 2023</p>
             <div className='wrap_allImages'>
                 <img src={fone1} className='each_photo_project first_photo_project'/>
                 <img src={fone1} className='each_photo_project' />
@@ -22,12 +22,12 @@ export const AllPhotos = () => {
                 <img src={fone1} className='each_photo_project' />
                 <img src={fone1} className='each_photo_project' />
                 <img src={fone1} className='each_photo_project' />
-                <img src={fone1} className='each_photo_project' />
-                <img src={fone1} className='each_photo_project' />
-                <img src={fone1} className='each_photo_project' />
-                <img src={fone1} className='each_photo_project' />
-                <img src={fone1} className='each_photo_project' />
-                <img src={fone1} className='each_photo_project' />
+                <img src={fone1} className='each_photo_project hideMobile' />
+                <img src={fone1} className='each_photo_project hideMobile' />
+                <img src={fone1} className='each_photo_project hideMobile' />
+                <img src={fone1} className='each_photo_project hideMobile' />
+                <img src={fone1} className='each_photo_project hideMobile' />
+                <img src={fone1} className='each_photo_project hideMobile' />
             </div>
           </div>
       </div>
